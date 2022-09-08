f20e4447ccde4577c0e2fc9250651437ce90404d
ItaloCobains <monokatchaleger@gmail.com>
2022-09-08 15:19:10 -0300

    List

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -2,29 +2,64 @@ import React from "react";
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 
-import { Container } from "./styles";
+import { Container, Content, Filters } from "./styles";
+import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 
 const List: React.FC = () => {
-  const options = [
+  const months = [
     {
-      value: "Italo",
-      label: "Italo",
+      value: 7,
+      label: "Julho",
     },
     {
-      value: "Italo",
-      label: "Italo",
+      value: 8,
+      label: "Agosto",
     },
     {
-      value: "Italo",
-      label: "Italo",
+      value: 9,
+      label: "Setembro",
+    },
+  ];
+
+  const years = [
+    {
+      value: 2022,
+      label: 2022,
+    },
+    {
+      value: 2021,
+      label: 2021,
+    },
+    {
+      value: 2020,
+      label: 2020,
     },
   ];
 
   return (
     <Container>
       <ContentHeader title="Saidas" lineColor="#e44c4e">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+        <button type="button" className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </Filters>
+
+      <Content>
+        <HistoryFinanceCard
+          tagColor="#E44C4E"
+          title="Conta de luz"
+          subtitle="08/09/2020"
+          amount="R$ 130,00"
+        />
+      </Content>
     </Container>
   );
 };
